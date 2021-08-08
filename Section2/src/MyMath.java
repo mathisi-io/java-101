34b6519f7c9098da0086f59d18141af822a4bda7
Aaron Niyonzima <aaron@mathisi.io>
2021-08-08 10:36:16 +0200

    Added Section2

--- Section2/src/MyMath.java
+++ Section2/src/MyMath.java
@@ -7,16 +7,15 @@ public class MyMath {
         a = 4;
         b = 12;
 
+        // type casting
         float div = (float) a / b;
-
         int num = (int)div;  // loss of info
-
-        System.out.println(num);
-
-        // System.out.println(a+b);
-        // System.out.println(a*b);
-        // System.out.println(div);
-        // System.out.println(a-b);
-        // System.out.println(a%b);
+        
+        System.out.println(a+b); //16
+        System.out.println(a*b); //48
+        System.out.println(div); //0.33333334
+        System.out.println(a-b); // -8 
+        System.out.println(a%b); // 4
+        System.out.println(num); // 0
     }
 }
